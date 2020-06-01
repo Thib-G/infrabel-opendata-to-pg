887dd160677528310463aa688d771432f83d431f
Thib <thibaut.g@gmail.com>
2020-06-01 11:54:56 +0200

    Add use strict

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,3 +1,4 @@
+"use strict";
 $(document).ready(function() {
   var mapboxKey = 'pk.eyJ1IjoidGhpYi1nIiwiYSI6ImNrOGdjeGJrYzAwYmwzbW1zd2htem11c2wifQ.SvlrQtH0K-uFxYe1Y-yhDg';
   var center = L.latLng([50.465841, 4.857634]);
